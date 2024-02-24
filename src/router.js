796029e1c317707a022930ef5963c0e88ebf9a2a
mackwick <mackenziewicker@gmail.com>
2024-02-24 17:18:27 -0500

    fixed card actions

--- src/router.js
+++ src/router.js
@@ -22,15 +22,12 @@ const router = createBrowserRouter(
     <Route path="/" element={<App />}>
       <Route path="" element={<Landing />} loader={deckLoader} />
       <Route path="/deck/:id" element={<Deckshow />} loader={cardLoader} />
-<<<<<<< HEAD
-=======
       <Route path="create" action={createDeckAction} />
       <Route path="update/:id" action={updateDeckAction} />
       <Route path="delete/:id" action={deleteDeckAction} />
       <Route path="create/card/:id" action={createCardAction} />
       <Route path="update/card/:id" action={updateCardAction} />
       <Route path="delete/card/:id" action={deleteCardAction} />
->>>>>>> 829bb1632221cddd2185c290b6050b2a92c72c29
     </Route>
   )
 );
