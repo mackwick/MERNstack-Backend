f139e89a178d5bcec6e9d6e4759f9b7b6cd46acd
mackwick <mackenziewicker@gmail.com>
2024-02-27 18:28:23 -0500

    added gear, edit, and delete icons -- not yet styled

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -20,19 +20,33 @@ const Landing = () => {
           <div key={deck._id} className="deck">
             {editMode && (
               <div>
-                <button onClick={() => setShowDropdown(showDropdown === deck._id ? null : deck._id)}>
-                  Gear Icon
-                </button>
+                <i
+                  class="fa-solid fa-gear"
+                  id="gear"
+                  onClick={() =>
+                    setShowDropdown(showDropdown === deck._id ? null : deck._id)
+                  }
+                ></i>
+
                 {showDropdown === deck._id && (
                   <div className="dropdown-menu">
-                    <Link to={`/edit/deck/${deck._id}`}>Edit Deck</Link>
-                    <button onClick={() => {/* handle delete deck */}}>Delete Deck</button>
-                    <Link to={`/deck/${deck._id}/manage-cards`}>Manage Cards</Link>
+                    <Link to={`/edit/deck/${deck._id}`}>
+                      <i class="fa-solid fa-pencil"></i>
+                    </Link>
+                    <i
+                      class="fa-solid fa-trash"
+                      onClick={() => {
+                        /* handle delete deck */
+                      }}
+                    ></i>
+                    <Link to={`/deck/${deck._id}/manage-cards`}>
+                      Manage Cards
+                    </Link>
                   </div>
                 )}
               </div>
             )}
-            <Link to={`/deck/${deck._id}`} style={{ textDecoration: 'none' }}>
+            <Link to={`/deck/${deck._id}`} style={{ textDecoration: "none" }}>
               <h3>{deck.name}</h3>
             </Link>
           </div>
