636319b11164c567d55a167164ddccf0b2b9ceaa
Roman Larionov <pcsaveddotcom@gmail.com>
2024-02-29 07:41:39 -0500

    manage cards fixed

--- src/pages/ManageCards.js
+++ src/pages/ManageCards.js
@@ -6,39 +6,39 @@ const ManageCards = () => {
   const location = useLocation();
   const [cards, setCards] = useState([]);
   
-    useEffect(() => {
-        const fetchDeck = async () => {
-            const response = await fetch(`${process.env.REACT_APP_URL}/deck/${deckId}`);
-            if(response.ok) {
-                const data = await response.json();
-                setCards(data.cards);
-            } else {
-                console.error("Failed to fetch deck with cards");
-            }
-        };
-        fetchDeck();
-    }, [deckId, location.state?.cardAdded]);
+  useEffect(() => {
+    const fetchDeck = async () => {
+      const response = await fetch(`${process.env.REACT_APP_URL}/deck/${deckId}`);
+      if (response.ok) {
+        const data = await response.json();
+        setCards(data.cards);
+      } else {
+        console.error("Failed to fetch deck with cards");
+      }
+    };
+    fetchDeck();
+  }, [deckId, location.state?.cardAdded]);
 
-    return (
-        <div className="manage-cards">
-            <h2>Manage Cards</h2>
-            {cards.length > 0 ? (
-                cards.map(card => (
-                    <div key={card._id} className="card-item">
-                        <p>Question: {card.question}</p>
-                        <p>Answer: {card.answer}</p>
-                                <div className="card-action-links">
-                                <Link to={``/edit/card/${card.deckId}/${card._id}``} className="edit-link">Edit</Link>
-                                <Link to={`/delete/card/${card.deckId}/${card._id}`} className="delete-link">Delete</Link>
-                                </div>
-                    </div>
-                ))
-            ) : (
-                <p>No cards available. Add some!</p>
-            )}
-            <Link to={`/create/card/${deckId}`} className="add-card-button">Add New Card</Link>
-        </div>
-    );
+  return (
+    <div className="manage-cards">
+      <h2>Manage Cards</h2>
+      {cards.length > 0 ? (
+        cards.map(card => (
+          <div key={card._id} className="card-item">
+            <p>Question: {card.question}</p>
+            <p>Answer: {card.answer}</p>
+            <div className="card-action-links">
+              <Link to={`/edit/card/${card.deckId}/${card._id}`} className="edit-link">Edit</Link>
+              <Link to={`/delete/card/${card.deckId}/${card._id}`} className="delete-link">Delete</Link>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>No cards available. Add some!</p>
+      )}
+      <Link to={`/create/card/${deckId}`} className="add-card-button">Add New Card</Link>
+    </div>
+  );
 };
 
 export default ManageCards;
